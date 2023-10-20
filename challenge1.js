d67e918e5cfff59b2d2ad9160c34a829cd28cd72
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2023-10-20 11:40:19 -0700

    add completed exercise

---
+++ challenge1.js
@@ -0,0 +1,18 @@
+/*
+Challenge 1
+Replace the blank underscores inside the console logs with the data type (i.e. 'string') you think each value will be. (Leave the quotes)
+Ex.
+var myStr = "hello"
+  console.log(typeof myStr === "string") //=> true
+ */
+
+//Uncomment the lines below to test your code
+ console.log(typeof 7.0 === "number") //true
+ console.log(typeof 22 === "number")  //true
+ console.log(typeof true === "boolean") //true
+
+var myVar;
+ console.log(typeof myVar === "undefined") //true
+
+myVar = "Kween";
+ console.log(typeof myVar === "string") //true
