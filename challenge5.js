a4c252389c5368a12db05378f44dac66e7df7b37
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2023-10-20 12:30:34 -0700

    add completed exercise

---
+++ challenge5.js
@@ -0,0 +1,18 @@
+/**
+ * 
+Challenge 5
+indexOf is a function that you can call on a string (string1) and accepts another string (string2) as an argument.
+It searches through string1 from left to right checking each character to see if it can find string2.
+If it finds string2 it returns the index position of the FIRST character that matches string2. If it cannot find string2 it returns negative one: -1.
+Enter the index position you think indexOf will return for each below.
+ */
+
+// console.log( indexOf("CodeSmith", "o") === 1)
+// console.log( indexOf("hello", "ll") === 2)
+// console.log( indexOf("zebra", "z") === 0)
+// console.log( indexOf("banana", "B") === -1)
+
+console.log("CodeSmith".indexOf("o") === 1); 
+console.log("hello".indexOf("ll") === 2); 
+console.log("zebra".indexOf("z") === 0); 
+console.log("banana".indexOf("B") === -1); 
