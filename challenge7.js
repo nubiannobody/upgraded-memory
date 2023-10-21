3f1f134bc3167de86e8dbede858616a1b2ef5f4f
Kween <kweenyoallhiada@gmail.com>
2023-10-20 21:13:16 -0700

    add cchallenge7.js file

---
+++ challenge7.js
@@ -0,0 +1,23 @@
+/**
+ Challenge 7
+Create a function isPrime that console.logs a boolean indicating if `number` is prime or not. 
+ */
+
+function isPrime(number) {
+   /* if (number > 1 && number % number === 0 && number % 1 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+  
+  }
+  */
+
+}
+  
+console.log(isPrime(-7)); // => false
+console.log(isPrime(2)); // => true
+console.log(isPrime(11)); // => true
+console.log(isPrime(15)); // => false
+
+  
